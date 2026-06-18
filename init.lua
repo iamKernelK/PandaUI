3502836af7ab32abd475c54a95079f1a3312ea8f
VunixUser <krarhader748@gmail.com>
2026-06-18 13:50:01 +0300

    Create PandaUI entry point module
    Initialize PandaUI module and set up component dependencies.

--- init.lua
+++ init.lua
@@ -1,1 +1,20 @@
+-- [[ PandaUI | Entry Point Module ]]
+local PandaUI = {}
 
+-- استدعاء المجلدات الفرعية
+local Modules = script:WaitForChild("Modules")
+local Components = script:WaitForChild("Components")
+local ElementsFolder = Modules:WaitForChild("Elements")
+
+-- ربط الملفات ببعضها
+local Theme = require(Modules:WaitForChild("Theme"))
+local Utils = require(Modules:WaitForChild("Utilities"))
+local Draggable = require(Modules:WaitForChild("Draggable"))
+local WindowModule = require(Components:WaitForChild("Window"))
+local TabModule = require(Components:WaitForChild("Tab"))
+
+function PandaUI:CreateWindow(Config)
+    return WindowModule.new(Config, Theme, Utils, Draggable, TabModule, ElementsFolder)
+end
+
+return PandaUI
